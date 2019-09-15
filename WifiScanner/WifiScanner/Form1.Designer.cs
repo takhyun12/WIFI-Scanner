a82ef7855b1765e252c8bb37bcc52127d34e816f
takhyun12 <41291493+takhyun12@users.noreply.github.com>
2019-09-15 15:20:59 +0900

    Add files via upload

---
+++ WifiScanner/WifiScanner/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace mook_WifiScanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvAP = new System.Windows.Forms.ListView();
+            this.chSSID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chQuality = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chEnabled = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chChanel = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chAlgorithm = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chAuth = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chMAC = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lvAP
+            // 
+            this.lvAP.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chSSID,
+            this.chQuality,
+            this.chEnabled,
+            this.chChanel,
+            this.chAlgorithm,
+            this.chAuth,
+            this.chMAC});
+            this.lvAP.FullRowSelect = true;
+            this.lvAP.GridLines = true;
+            this.lvAP.Location = new System.Drawing.Point(12, 12);
+            this.lvAP.Name = "lvAP";
+            this.lvAP.Size = new System.Drawing.Size(660, 440);
+            this.lvAP.TabIndex = 4;
+            this.lvAP.UseCompatibleStateImageBehavior = false;
+            this.lvAP.View = System.Windows.Forms.View.Details;
+            // 
+            // chSSID
+            // 
+            this.chSSID.Text = "이름";
+            this.chSSID.Width = 100;
+            // 
+            // chQuality
+            // 
+            this.chQuality.Text = "신호강도";
+            // 
+            // chEnabled
+            // 
+            this.chEnabled.Text = "암호화";
+            // 
+            // chChanel
+            // 
+            this.chChanel.Text = "채널";
+            this.chChanel.Width = 58;
+            // 
+            // chAlgorithm
+            // 
+            this.chAlgorithm.Text = "암호방식";
+            // 
+            // chAuth
+            // 
+            this.chAuth.Text = "인증방식";
+            this.chAuth.Width = 100;
+            // 
+            // chMAC
+            // 
+            this.chMAC.Text = "MAC";
+            this.chMAC.Width = 174;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 468);
+            this.Controls.Add(this.lvAP);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "무선네트워크 탐지프로그램";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvAP;
+        private System.Windows.Forms.ColumnHeader chSSID;
+        private System.Windows.Forms.ColumnHeader chQuality;
+        private System.Windows.Forms.ColumnHeader chEnabled;
+        private System.Windows.Forms.ColumnHeader chChanel;
+        private System.Windows.Forms.ColumnHeader chAlgorithm;
+        private System.Windows.Forms.ColumnHeader chAuth;
+        private System.Windows.Forms.ColumnHeader chMAC;
+    }
+}
+
---
+++ WifiScanner/WifiScanner/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using NativeWifi;
+
+namespace mook_WifiScanner
+{
+    public partial class Form1 : Form
+    {
+        WlanClient wlanClient = new WlanClient();
+        Thread thrAP;
+        private delegate void OnWifiViewDelegate(bool flags, object[] AddWifi); //델리게이트 선언
+        private OnWifiViewDelegate OnView = null;  //델리게이트 개체 생성
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (wlanClient.Interfaces.Count() == 0)
+            {
+                // 인터페이스가 없음. 프로그램 종료
+                MessageBox.Show("단말기 내 무선 랜카드가 없어 탐지가 불가능합니다.", "인터페이스 오류");
+                return;
+            }
+            OnView = new OnWifiViewDelegate(OnWifiList);
+            thrAP = new Thread(ThreadList);
+            thrAP.Start();
+        }
+
+        private void ThreadList()
+        {
+            while (true)
+            {
+                OnView.Invoke(true, null);
+
+                Wlan.WlanAvailableNetwork[] wlanBssEntries = 
+                    wlanClient.Interfaces[0].GetAvailableNetworkList(0);
+                foreach (Wlan.WlanAvailableNetwork network in wlanBssEntries)
+                {
+                    object[] AddWifi = new object[7] { 
+                        network.dot11Ssid, 
+                        network.wlanSignalQuality.ToString(), 
+                        network.securityEnabled.ToString(), 
+                        network.dot11Ssid.SSID, 
+                        network.dot11DefaultCipherAlgorithm.ToString(), 
+                        network.dot11DefaultAuthAlgorithm.ToString(), 
+                        network.dot11Ssid.SSID };
+
+                    OnView.Invoke(false, AddWifi);
+                }
+                Thread.Sleep(10000);
+            }
+        }
+
+        private void OnWifiList(bool flags, object[] AddWifi)
+        {
+            if (flags == true)
+                this.lvAP.Items.Clear();
+            else
+            {
+                var lvt = new ListViewItem(new string[] { 
+                        GetStringForSSID((Wlan.Dot11Ssid)AddWifi[0]), 
+                        AddWifi[1].ToString(), 
+                        AddWifi[2].ToString(), 
+                        GetMacChanel(1, ConvertToMAC((byte[])AddWifi[3])), 
+                        AddWifi[4].ToString(), 
+                        AddWifi[5].ToString(), 
+                        GetMacChanel(2, ConvertToMAC((byte[])AddWifi[6])) });
+                this.lvAP.Items.Add(lvt);
+            }
+        }
+
+        private string GetMacChanel(int i, string Name)
+        {
+            Wlan.WlanBssEntry[] lstWlanBss = 
+                wlanClient.Interfaces[0].GetNetworkBssList();
+            var reAP = "";
+            foreach (var oWlan in lstWlanBss)
+            {
+                if (i == 2)
+                {
+                    if (ConvertToMAC(oWlan.dot11Ssid.SSID) == Name)
+                    {
+                        reAP = ConvertToMAC(oWlan.dot11Bssid);
+                    }
+                }
+                else if (i == 1)
+                {
+                    if (ConvertToMAC(oWlan.dot11Ssid.SSID) == Name)
+                    {
+                        var chnl = oWlan.chCenterFrequency.ToString();
+                        switch (chnl)
+                        {
+                            case "2412000":
+                                reAP = "1";
+                                break;
+                            case "2417000":
+                                reAP = "2";
+                                break;
+                            case "2422000":
+                                reAP = "3";
+                                break;
+                            case "2427000":
+                                reAP = "4";
+                                break;
+                            case "2432000":
+                                reAP = "5";
+                                break;
+                            case "2437000":
+                                reAP = "6";
+                                break;
+                            case "2442000":
+                                reAP = "7";
+                                break;
+                            case "2447000":
+                                reAP = "8";
+                                break;
+                            case "2452000":
+                                reAP = "9";
+                                break;
+                            case "2457000":
+                                reAP = "10";
+                                break;
+                            case "2462000":
+                                reAP = "11";
+                                break;
+                            case "2467000":
+                                reAP = "12";
+                                break;
+                            case "2472000":
+                                reAP = "13";
+                                break;
+                        }
+                    }
+
+                }
+            }
+            return reAP;
+        }
+        string ConvertToMAC(byte[] MAC)
+        {
+            string strMAC = "";
+            for (int index = 0; index < 6; index++)
+                strMAC += MAC[index].ToString("X2") + "-";
+            return strMAC.Substring(0, strMAC.Length - 1);
+        }
+
+        static string GetStringForSSID(Wlan.Dot11Ssid ssid)
+        {
+            return Encoding.ASCII.GetString(ssid.SSID, 
+                0, (int)ssid.SSIDLength);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.thrAP != null)
+                thrAP.Abort();
+            Application.ExitThread();
+        }
+    }
+}
